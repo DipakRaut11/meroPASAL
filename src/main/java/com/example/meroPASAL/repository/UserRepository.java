4c13ffffdfdd53f0759ae6cdbd76d4f84cdf81f6
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-14 16:39:02 +0545

    add check if user is exist by mail or not

--- src/main/java/com/example/meroPASAL/repository/UserRepository.java
+++ src/main/java/com/example/meroPASAL/repository/UserRepository.java
@@ -1,2 +1,11 @@
-package com.example.meroPASAL.repository;public interface UserRepository {
+package com.example.meroPASAL.repository;
+
+import com.example.meroPASAL.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
