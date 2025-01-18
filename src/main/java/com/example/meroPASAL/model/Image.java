b6a8574f3d817dd9f9639ddd68717e7e51b35909
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:58:14 +0545

    Image table in database for image storage

--- src/main/java/com/example/meroPASAL/model/Image.java
+++ src/main/java/com/example/meroPASAL/model/Image.java
@@ -1,2 +1,30 @@
-package com.example.meroPASAL.model;public class Image {
+package com.example.meroPASAL.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "product_id"
+    )
+    @JsonIgnore
+    private Product product;
 }
