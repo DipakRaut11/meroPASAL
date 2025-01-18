8416a7a0cd43033f771a55606a181022b1723cba
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:53:00 +0545

    create product repo for interaction with product table

--- src/main/java/com/example/meroPASAL/Repository/ProductRepo.java
+++ src/main/java/com/example/meroPASAL/Repository/ProductRepo.java
@@ -1,2 +1,24 @@
-package com.example.meroPASAL.Repository;public interface ProductRepo {
+package com.example.meroPASAL.Repository;
+
+import com.example.meroPASAL.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+
+    boolean existsByNameAndBrand(String name, String brand);
+
+    List<Product> findByCategory_Name(String categoryName);
+
+    List<Product> findByBrand(String brand);
+
+    // Updated query method
+    List<Product> findProductByBrandAndCategory_Name(String brand, String categoryName);
+
+    List<Product> findProductByName(String name);
+
+    List<Product> findProductByBrandAndName(String brand, String name);
+
+    Long countByBrandAndName(String brand, String name);
 }
