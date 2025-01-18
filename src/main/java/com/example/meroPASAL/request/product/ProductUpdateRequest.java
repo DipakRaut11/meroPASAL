9de16bd5caa6aa43d2f1895dda4df104f73dce59
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:59:52 +0545

    build request for product update

--- src/main/java/com/example/meroPASAL/request/product/ProductUpdateRequest.java
+++ src/main/java/com/example/meroPASAL/request/product/ProductUpdateRequest.java
@@ -1,2 +1,17 @@
-package com.example.meroPASAL.service.product;public class ProductUpdateRequest {
+package com.example.meroPASAL.request.product;
+
+import com.example.meroPASAL.model.Category;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductUpdateRequest {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+    private Category category;
 }
