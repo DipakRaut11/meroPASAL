ad867f8a90e8c833d7b68e0a1709b58268959985
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:59:28 +0545

    build request for product add

--- src/main/java/com/example/meroPASAL/request/product/AddProductRequest.java
+++ src/main/java/com/example/meroPASAL/request/product/AddProductRequest.java
@@ -1,2 +1,17 @@
-package com.example.meroPASAL.request.product;public class AddProductRequest {
+package com.example.meroPASAL.request.product;
+
+import com.example.meroPASAL.model.Category;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class AddProductRequest {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+    private Category category;
 }
