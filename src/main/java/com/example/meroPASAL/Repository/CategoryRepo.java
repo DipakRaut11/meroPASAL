294a4cc146596c17234bd7fdbbb8af504f1af074
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:51:16 +0545

    create category repo for intersection with db

--- src/main/java/com/example/meroPASAL/Repository/CategoryRepo.java
+++ src/main/java/com/example/meroPASAL/Repository/CategoryRepo.java
@@ -1,2 +1,10 @@
-package com.example.meroPASAL.Repository;public interface CategoryRepo {
+package com.example.meroPASAL.Repository;
+
+import com.example.meroPASAL.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepo extends JpaRepository<Category, Long> {
+
+    Category findByName(String name);
+    boolean existsByName(String name);
 }
