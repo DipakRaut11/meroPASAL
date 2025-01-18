afc8eb42b00222b39b5576829bdcef9a6810a369
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:57:34 +0545

    Category table for database

--- src/main/java/com/example/meroPASAL/model/Category.java
+++ src/main/java/com/example/meroPASAL/model/Category.java
@@ -1,2 +1,30 @@
-package com.example.meroPASAL.model;public class Category {
+package com.example.meroPASAL.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products;
+
+    public Category(String name) {
+        this.name = name;
+    }
 }
