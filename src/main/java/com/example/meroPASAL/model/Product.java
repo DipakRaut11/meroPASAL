2e0386c001afbb5b6f3af26e2b80d330493048fb
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:58:46 +0545

    Add json ignore at category

--- src/main/java/com/example/meroPASAL/model/Product.java
+++ src/main/java/com/example/meroPASAL/model/Product.java
@@ -1,2 +1,62 @@
-package com.example.meroPASAL.model;public class Product {
+package com.example.meroPASAL.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Product name is required")
+    private String name;
+
+    @NotBlank(message = "Brand name is required")
+    private String brand;
+
+    @Positive(message = "Price must be positive")
+    private BigDecimal price;
+
+    @Positive(message = "Inventory must be positive")
+    private int inventory;
+
+    private String description;
+
+    @JsonIgnore
+    @ManyToOne(
+            cascade = CascadeType.ALL
+    )
+    @JoinColumn(name = "category_id") // Foreign key mapping
+    private Category category;
+
+    @OneToMany(
+            mappedBy = "product",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    @JsonIgnore
+    private List<Image> images;
+
+
+    public Product(String name, String brand, BigDecimal price, int inventory, String description, Category category) {
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.inventory = inventory;
+        this.description = description;
+        this.category = category;
+    }
 }
