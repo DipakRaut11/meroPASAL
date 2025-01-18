fa6546ad2d7bdf570a59eef3c2a8006b5d2e526a
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:56:40 +0545

    Exception for products

--- src/main/java/com/example/meroPASAL/exception/ResourceAlreadyExistException.java
+++ src/main/java/com/example/meroPASAL/exception/ResourceAlreadyExistException.java
@@ -1,2 +1,7 @@
-package com.example.meroPASAL.exception;public class ResourceAlreadyExistException {
+package com.example.meroPASAL.exception;
+
+public class ResourceAlreadyExistException extends RuntimeException {
+    public ResourceAlreadyExistException(String message) {
+        super(message);
+    }
 }
