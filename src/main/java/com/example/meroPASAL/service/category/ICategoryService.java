97d0a8b309821c4be21bb2ade2cf4285ab793b82
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:47:51 +0545

    add category interface for service

--- src/main/java/com/example/meroPASAL/service/category/ICategoryService.java
+++ src/main/java/com/example/meroPASAL/service/category/ICategoryService.java
@@ -1,2 +1,14 @@
-package com.example.meroPASAL.service.category;public interface ICategoryService {
+package com.example.meroPASAL.service.category;
+
+import com.example.meroPASAL.model.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category getCategoryById(Long id);
+    void deleteCategory(Long id);
+    Category addCategory(Category category);
+    Category updateCategory(Category category, Long categoryId);
+    List<Category> getAllCategories();
+    Category getCategoriesByName(String name);
 }
