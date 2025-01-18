1b99de2fc678a4609156e32c326dfcf0d2c2e611
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:52:17 +0545

    image repo for interaction with image table

--- src/main/java/com/example/meroPASAL/Repository/ImageRepos.java
+++ src/main/java/com/example/meroPASAL/Repository/ImageRepos.java
@@ -1,2 +1,10 @@
-package com.example.meroPASAL.Repository;public interface ImageRepos {
+package com.example.meroPASAL.Repository;
+
+import com.example.meroPASAL.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ImageRepos extends JpaRepository<Image, Long> {
+    List<Image> findByProduct_Id(Long id);
 }
