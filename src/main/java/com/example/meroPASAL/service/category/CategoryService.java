1be94778ca49d460574a5947e0460429a768f871
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-18 16:48:25 +0545

    create service using category interface

--- src/main/java/com/example/meroPASAL/service/category/CategoryService.java
+++ src/main/java/com/example/meroPASAL/service/category/CategoryService.java
@@ -1,2 +1,37 @@
-package com.example.meroPASAL.service.category;public class CategoryService {
+package com.example.meroPASAL.service.category;
+
+import com.example.meroPASAL.model.Category;
+
+import java.util.List;
+
+public class CategoryService implements ICategoryService{
+    @Override
+    public Category getCategoryById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        return null;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return List.of();
+    }
+
+    @Override
+    public Category getCategoriesByName(String name) {
+        return null;
+    }
 }
