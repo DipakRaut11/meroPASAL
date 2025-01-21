671ddb46e63844e629f33cb1df4f2a9c75ceecec
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-21 09:29:27 +0545

    implement intrface of category service for building business logic

--- src/main/java/com/example/meroPASAL/service/category/CategoryService.java
+++ src/main/java/com/example/meroPASAL/service/category/CategoryService.java
@@ -1,37 +1,62 @@
 package com.example.meroPASAL.service.category;
 
+import com.example.meroPASAL.Repository.CategoryRepo;
+import com.example.meroPASAL.exception.ResourceAlreadyExistException;
+import com.example.meroPASAL.exception.ResourceNotFoundException;
 import com.example.meroPASAL.model.Category;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
+@RequiredArgsConstructor
 public class CategoryService implements ICategoryService{
+
+    private final CategoryRepo categoryRepo;
+    private final ModelMapper modelMapper;
+
     @Override
     public Category getCategoryById(Long id) {
-        return null;
+        return categoryRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found with id " + id));
     }
 
     @Override
-    public void deleteCategory(Long id) {
+    public void deleteCategoryById(Long id) {
+        categoryRepo.findById(id)
+                .ifPresentOrElse(category -> categoryRepo.deleteById(id),
+                        () -> new ResourceNotFoundException("Category not found with id " + id));
 
     }
 
     @Override
     public Category addCategory(Category category) {
-        return null;
+         return Optional.of(category)
+                .filter(c -> !categoryRepo.existsByName(c.getName()))
+                .map(categoryRepo::save)
+                .orElseThrow(()-> new ResourceAlreadyExistException(category.getName()+" Category already exists"));
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        return null;
+        return Optional.ofNullable(getCategoryById(categoryId))
+                .map(oldCategory -> {
+                    oldCategory.setName(category.getName());
+                    return categoryRepo.save(oldCategory);
+                })
+                .orElseThrow(()-> new ResourceNotFoundException("Category not found"));
     }
 
     @Override
     public List<Category> getAllCategories() {
-        return List.of();
+        return categoryRepo.findAll();
     }
 
     @Override
     public Category getCategoriesByName(String name) {
-        return null;
+        return categoryRepo.findByName(name);
     }
 }
