ee3d0e5c1cf18d4703522b10bf1266d7da52050a
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-22 16:39:21 +0545

    build user dto

--- src/main/java/com/example/meroPASAL/dto/user/UserDto.java
+++ src/main/java/com/example/meroPASAL/dto/user/UserDto.java
@@ -1,18 +1,18 @@
-package com.dipakraut.eCommerce.dto.user;
-
-import com.dipakraut.eCommerce.dto.cart.CartDto;
-import com.dipakraut.eCommerce.dto.order.OrderDto;
-import lombok.Data;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Data
-public class UserDto {
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private List<OrderDto> orders = new ArrayList<>();
-    private CartDto cart;
-}
+//package com.example.meroPASAL.dto.user;
+//
+//import com.dipakraut.eCommerce.dto.cart.CartDto;
+//import com.dipakraut.eCommerce.dto.order.OrderDto;
+//import lombok.Data;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Data
+//public class UserDto {
+//    private Long id;
+//    private String firstName;
+//    private String lastName;
+//    private String email;
+////    private List<OrderDto> orders = new ArrayList<>();
+//   // private CartDto cart;
+//}
