854a14646ebe3d842e616167d4ebc415a20fc6ec
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-22 16:22:01 +0545

    build CartItemService abstracting method from ICartItemService

--- src/main/java/com/example/meroPASAL/service/cart/CartService.java
+++ src/main/java/com/example/meroPASAL/service/cart/CartService.java
@@ -1,2 +1,63 @@
-package com.example.meroPASAL.service.cart;public class CartService {
+package com.example.meroPASAL.service.cart;
+
+import com.example.meroPASAL.Repository.cart.CartItemRepository;
+import com.example.meroPASAL.Repository.cart.CartRepository;
+import com.example.meroPASAL.exception.ResourceNotFoundException;
+import com.example.meroPASAL.model.Cart;
+import com.example.meroPASAL.security.userModel.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CartService implements ICartService{
+
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+
+    @Override
+    public Cart getCartById(Long id) {
+        Cart cart = cartRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart not found with id: " + id));
+        BigDecimal totalAmount =cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+        return cartRepository.save(cart);
+    }
+
+    @Transactional
+    @Override
+    public void clearCart(Long id) {
+        Cart cart = getCartById(id);
+        cartItemRepository.deleteAllByCartId(id);
+        cart.getItems().clear();
+        cartRepository.deleteById(id);
+
+    }
+
+    @Override
+    public BigDecimal getCartTotalPrice(Long id) {
+        Cart cart =getCartById(id);
+        return cart.getTotalAmount();
+    }
+
+    @Override
+    public Cart initializeNewCart(User user) {
+        return Optional.ofNullable(getCartByUserId(user.getId()))
+                .orElseGet(() -> {
+                    Cart cart = new Cart();
+                    cart.setUser(user);
+                    return cartRepository.save(cart);
+                });
+    }
+
+    @Override
+    public Cart getCartByUserId(Long userId) {
+
+            return cartRepository.findByUserId(userId);
+    }
 }
