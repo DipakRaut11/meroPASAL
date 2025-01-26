5fd406ca534907841cef1dd9377cfdb080268f79
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-26 14:55:56 +0545

    build OrderItem entity

--- src/main/java/com/example/meroPASAL/model/OrderItem.java
+++ src/main/java/com/example/meroPASAL/model/OrderItem.java
@@ -1,6 +1,5 @@
-package com.dipakraut.eCommerce.model;
+package com.example.meroPASAL.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
