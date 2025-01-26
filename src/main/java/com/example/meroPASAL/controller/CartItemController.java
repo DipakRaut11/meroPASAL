137f9707d2e93848f23f122ef8c7e2ac38c74943
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-26 14:06:13 +0545

    add authorization in class

--- src/main/java/com/example/meroPASAL/controller/CartItemController.java
+++ src/main/java/com/example/meroPASAL/controller/CartItemController.java
@@ -1,2 +1,67 @@
-package com.example.meroPASAL.controller;public class CartItemController {
+package com.example.meroPASAL.controller;
+
+import com.example.meroPASAL.exception.ResourceNotFoundException;
+import com.example.meroPASAL.model.Cart;
+import com.example.meroPASAL.model.CartItem;
+import com.example.meroPASAL.model.Product;
+import com.example.meroPASAL.response.ApiResponse;
+import com.example.meroPASAL.security.service.AuthenticationService;
+import com.example.meroPASAL.security.userModel.User;
+import com.example.meroPASAL.service.cart.CartService;
+import com.example.meroPASAL.service.cart.ICartItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/cartItem")
+@PreAuthorize("hasRole('ROLE_CUSTOMER')")
+public class CartItemController {
+
+    private final ICartItemService cartItemService;
+   private final AuthenticationService userService;
+    private final CartService cartService;
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addItemToCart(
+            @RequestParam Long productId,
+            @RequestParam Integer quantity) {
+        User user = userService.getAuthenticatedUser();
+        Cart cart = cartService.initializeNewCart(user);
+
+        cartItemService.addItemToCart(cart.getId(), productId, quantity);
+        return ResponseEntity.ok(new ApiResponse("Item added to cart successfully", null));
+
+
+    }
+
+    @DeleteMapping("/cart/{cartId}/item/{itemId}/remove")
+    public ResponseEntity<ApiResponse> removeItemFromCart(@PathVariable Long cartId, @PathVariable Long itemId) {
+        try {
+            cartItemService.removeItemFromCart(cartId, itemId);
+            return ResponseEntity.ok(new ApiResponse("Item removed from cart successfully", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+
+    }
+
+    @PutMapping("/cart/{cartId}/item/{itemId}/update")
+    public ResponseEntity<ApiResponse> updateCartItemQuantity(@PathVariable Long cartId, @PathVariable Long itemId, @RequestParam Integer quantity) {
+        try {
+            cartItemService.updateCartItemQuantity(cartId, itemId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Item quantity updated successfully", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+
+    }
+
 }
