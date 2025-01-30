9d7cc8e20dbe8bbd3b10368441fc04c353ab47a6
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-01-30 17:45:19 +0545

    Remove CrossOrigin because CORS has been globally configured in WebConfig class

--- src/main/java/com/example/meroPASAL/security/controller/AuthenticationController.java
+++ src/main/java/com/example/meroPASAL/security/controller/AuthenticationController.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("${api.prefix}/auth")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 @RequiredArgsConstructor
 public class AuthenticationController {
 
