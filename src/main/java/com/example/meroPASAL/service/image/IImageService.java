f34e0bfb7d1590c3212cb2ac2cee7de0560cfcf9
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-03-26 09:34:13 +0545

    Add IImageService interface for image handling

--- src/main/java/com/example/meroPASAL/service/image/IImageService.java
+++ src/main/java/com/example/meroPASAL/service/image/IImageService.java
@@ -1,2 +1,16 @@
-package com.example.meroPASAL.service.image;public interface IImageService {
+package com.example.meroPASAL.service.image;
+
+import com.example.meroPASAL.dto.ImageDto;
+import com.example.meroPASAL.model.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IImageService {
+
+    Image getImageById(Long id);
+    void deleteImageById(Long id);
+    List<ImageDto> saveImages(List<MultipartFile> files, Long productId);
+    void updateImage(MultipartFile file, Long imageId);
+
 }
