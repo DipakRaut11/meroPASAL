8f1df520d595f54667d0f96df68aa7f6f8c4a410
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-04-08 12:56:53 +0545

    update ImageDto with imageId, imageName, and downloadUrl fields

--- src/main/java/com/example/meroPASAL/dto/ImageDto.java
+++ src/main/java/com/example/meroPASAL/dto/ImageDto.java
@@ -1,4 +1,16 @@
 package com.example.meroPASAL.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class ImageDto {
+    private Long imageId;
+    private String imageName;
+    private String downloadUrl;
 }
