6015e1b02e5be56c2f607c99194267274fac5c24
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-04-08 12:59:11 +0545

    add ImageProcessingException method

--- src/main/java/com/example/meroPASAL/exception/ImageProcessingException.java
+++ src/main/java/com/example/meroPASAL/exception/ImageProcessingException.java
@@ -1,2 +1,8 @@
-package com.example.meroPASAL.exception;public class ImageProcessingException {
+package com.example.meroPASAL.exception;
+
+public class ImageProcessingException extends RuntimeException {
+
+    public ImageProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
