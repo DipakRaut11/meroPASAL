edb6c5cb9e784135f6cd2e97dc67c4994a722bd4
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-08-14 15:49:20 +0545

    update add item to cart

--- src/main/java/com/example/meroPASAL/service/cart/CartItemService.java
+++ src/main/java/com/example/meroPASAL/service/cart/CartItemService.java
@@ -1,15 +1,12 @@
 package com.example.meroPASAL.service.cart;
 
-import com.example.meroPASAL.Repository.ProductRepo;
 import com.example.meroPASAL.Repository.cart.CartItemRepository;
 import com.example.meroPASAL.Repository.cart.CartRepository;
 import com.example.meroPASAL.exception.ResourceNotFoundException;
 import com.example.meroPASAL.model.Cart;
 import com.example.meroPASAL.model.CartItem;
-import com.example.meroPASAL.model.Category;
 import com.example.meroPASAL.model.Product;
 import com.example.meroPASAL.service.product.IProductService;
-import com.example.meroPASAL.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,40 +14,33 @@ import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
-public class CartItemService implements ICartItemService{
+public class CartItemService implements ICartItemService {
 
     private final CartItemRepository cartItemRepository;
     private final ICartService cartService;
     private final IProductService productService;
     private final CartRepository cartRepository;
 
-
     @Override
     public void addItemToCart(Long cartId, Long productId, int quantity) {
-        // get the cart
-        // get the product
-        //check if the product already in the cart
-        // if yes then increase the quantity with the requested quentity
-        //if no, then initiate a new cartItem entry
-
         Cart cart = cartService.getCartById(cartId);
         Product product = productService.getProductById(productId);
+
         CartItem cartItem = cart.getItems()
                 .stream()
                 .filter(item -> item.getProduct().getId().equals(productId))
-                .findFirst().orElse(new CartItem());
+                .findFirst()
+                .orElse(new CartItem());
 
         if (cartItem.getId() == null) {
             cartItem.setCart(cart);
             cartItem.setProduct(product);
             cartItem.setQuantity(quantity);
             cartItem.setUnitPrice(product.getPrice());
-
-
-        }
-        else {
+        } else {
             cartItem.setQuantity(cartItem.getQuantity() + quantity);
         }
+
         cartItem.setTotalPrice();
         cart.addItem(cartItem);
         cartItemRepository.save(cartItem);
@@ -68,16 +58,16 @@ public class CartItemService implements ICartItemService{
     @Override
     public void updateCartItemQuantity(Long cartId, Long productId, int quantity) {
         Cart cart = cartService.getCartById(cartId);
+
         cart.getItems()
                 .stream()
                 .filter(item -> item.getProduct().getId().equals(productId))
                 .findFirst()
                 .ifPresent(item -> {
-                            item.setQuantity(quantity);
-                            item.setUnitPrice(item.getProduct().getPrice());
-                            item.setTotalPrice();
-                        }
-                );
+                    item.setQuantity(quantity);
+                    item.setUnitPrice(item.getProduct().getPrice());
+                    item.setTotalPrice();
+                });
 
         BigDecimal totalAmount = cart.getItems().stream()
                 .map(CartItem::getTotalPrice)
@@ -90,8 +80,9 @@ public class CartItemService implements ICartItemService{
     public CartItem getCartItem(Long cartId, Long productId) {
         Cart cart = cartService.getCartById(cartId);
         return cart.getItems()
-                .stream().
-                filter(item -> item.getProduct().getId().equals(productId))
-                .findFirst().orElseThrow(() -> new ResourceNotFoundException("Item not found"));
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Item not found in cart"));
     }
 }
