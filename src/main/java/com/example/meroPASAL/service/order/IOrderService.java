415c66e525d527128c573b7696f0cf82b42ed734
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-08-20 21:48:35 +0545

    feat(order): declare updatePaymentStatus method in IOrderService

--- src/main/java/com/example/meroPASAL/service/order/IOrderService.java
+++ src/main/java/com/example/meroPASAL/service/order/IOrderService.java
@@ -3,7 +3,9 @@ package com.example.meroPASAL.service.order;
 
 
 import com.example.meroPASAL.dto.order.OrderDto;
+import com.example.meroPASAL.dto.order.OrderSummaryDto;
 import com.example.meroPASAL.enums.OderStatus;
+import com.example.meroPASAL.enums.PaymentStatus;
 import com.example.meroPASAL.model.Order;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,4 +26,7 @@ public interface IOrderService {
 
     List<OrderDto> getOrdersByShop(Long shopId);
     OrderDto updateOrderStatus(Long orderId, OderStatus status, Long shopId);
+
+    List<OrderSummaryDto> getAllOrders();
+    OrderSummaryDto updatePaymentStatus(Long orderId, PaymentStatus status);
 }
