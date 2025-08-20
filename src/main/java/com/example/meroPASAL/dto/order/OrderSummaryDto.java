3e1b68c2b356f85df8f6d50cd5ce3cac8826d8b8
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-08-20 21:52:12 +0545

    feat(order): add paymentStatus field to OrderSummaryDto

--- src/main/java/com/example/meroPASAL/dto/order/OrderSummaryDto.java
+++ src/main/java/com/example/meroPASAL/dto/order/OrderSummaryDto.java
@@ -1,2 +1,28 @@
-package com.example.meroPASAL.dto.order;public class OrderSummaryDto {
+package com.example.meroPASAL.dto.order;
+
+
+import com.example.meroPASAL.enums.OderStatus;
+import com.example.meroPASAL.enums.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderSummaryDto {
+    private Long id;
+    private Long userId;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+    private OderStatus orderStatus;
+    private String paymentScreenshotUrl; // ✅ proof image
+    private List<OrderItemDto> items;
+    private PaymentStatus paymentStatus;
+
+
 }
