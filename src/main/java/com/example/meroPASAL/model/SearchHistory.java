db7f3c51ea4aa2a02a837d2cf7404d5ebc0418e2
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-09-01 14:43:00 +0545

    add entity for searchHistory

--- src/main/java/com/example/meroPASAL/model/SearchHistory.java
+++ src/main/java/com/example/meroPASAL/model/SearchHistory.java
@@ -1,2 +1,31 @@
-package com.example.meroPASAL.model;public class SearchHistory {
+package com.example.meroPASAL.model;
+
+import com.example.meroPASAL.security.userModel.Customer;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class SearchHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String searchType;   // "name", "brand", "category"
+    private String searchValue;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    public SearchHistory(String searchType, String searchValue, Customer customer) {
+        this.searchType = searchType;
+        this.searchValue = searchValue;
+        this.customer = customer;
+    }
 }
