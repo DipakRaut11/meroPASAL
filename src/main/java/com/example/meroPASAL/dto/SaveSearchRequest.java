a2996738b3c704f635b1e0a356b8d937ef427e16
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-09-01 14:44:44 +0545

    build save search request dto

--- src/main/java/com/example/meroPASAL/dto/SaveSearchRequest.java
+++ src/main/java/com/example/meroPASAL/dto/SaveSearchRequest.java
@@ -1,4 +1,8 @@
 package com.example.meroPASAL.dto;
+import lombok.Data;
 
+@Data
 public class SaveSearchRequest {
+    private String searchType;   // "name", "brand", "category"
+    private String searchValue;
 }
